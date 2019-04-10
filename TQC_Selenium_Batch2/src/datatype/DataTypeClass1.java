7a85a5cbd757b6efdb13aaf6042bb2d8d12d8e1f
ROHBA_000 <ROHBA_000@Rohit>
2019-04-10 07:45:16 +0530

    First Commit

---
+++ TQC_Selenium_Batch2/src/datatype/DataTypeClass1.java
@@ -0,0 +1,25 @@
+package datatype;
+
+public class DataTypeClass1 {
+
+	static byte b;
+	static short s;
+	static int x;
+	static long l;
+	static double d;
+	static float f;
+	static char c;
+	static boolean bo;
+	
+	public static void main(String[] args) {
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(x);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(bo);
+		
+	}
+}
